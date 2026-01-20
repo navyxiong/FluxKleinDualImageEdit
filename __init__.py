2df050fd5fb924bf39ab72cbe03d8f135988ad5c
CHIEF AI <19092813@qq.com>
2026-01-20 10:04:27 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-# __init__.py
-from .flux_klein_dual_edit import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .flux_klein_triple_conditioning import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
