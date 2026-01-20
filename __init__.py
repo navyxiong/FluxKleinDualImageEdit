2017c2a88d611b62b711faa226e2a0c523b9f5b7
CHIEF AI <19092813@qq.com>
2026-01-20 10:34:08 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@
 from .flux_klein_triple_conditioning import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+print("[Flux Klein Triple] Module loaded successfully!")
